18960cf76bb52beff20bb42767a17c747d97841a
pancho horrillo <pancho@pancho.name>
2023-04-12 22:25:19 +0200

    [Keymap] Add personal keymap for a Keychron Q8
    - Caps Lock key is now Control, but when tapped, it is Esc.
    - Caps Lock gets evicted to an upper layer.
    - Left Shift also yields an ( when tapped.
    - Right Shift also yields an ) when tapped.
    - The Mac layer is a Windows layer, but Left Control is not multiplexed
    with Escape.  Escape is where Tab is.  Tab is Control-i.  Shifts are not
    combined with parentheses.
    - The two keys right under the encoder are now PageUp and PageDown.
    - Delete is now on FN layers, on Backspace.
    - Fn-Control yields R-Control
    - Fn-Win yields R-Win
    - TODO: consider dropping all RGB madness, or at least relegating it to
    the Mac (Windows) layer.
    - Now, for each base layer, both fn keys will activate the same
      alternative layer, so that F1-F11 keys are accessible using proper
      both-hand chords.  Multimedia keys have been sacrificed to achieve this.
    
    Many thanks to Yari for his assistance!

---
+++ keyboards/keychron/q8/ansi_encoder/keymaps/panchoh/config.h
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * Copyright © 2023-2025 pancho horrillo <pancho@pancho.name>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+// https://docs.qmk.fm/#/tap_hold?id=hold-on-other-key-press
+#define HOLD_ON_OTHER_KEY_PRESS
+
+// https://docs.qmk.fm/#/tap_hold?id=retro-tapping
+#define RETRO_TAPPING
+
+// Left Control when pressed, Escape when tapped.
+#define CTL_ESC_T LCTL_T(KC_ESC)
+
+// https://docs.qmk.fm/#/feature_grave_esc
+#define GRAVE_ESC_CTRL_OVERRIDE
+
+// overrides go here!
