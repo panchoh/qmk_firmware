bcf9cdf966d4f346377c038084d43f94bf63558f
pancho horrillo <pancho@pancho.name>
2024-08-03 10:58:10 +0200

    [Keymap] Add a personal keymap for a Ploopy Thumb

---
+++ keyboards/ploopyco/trackball_thumb/keymaps/panchoh/config.h
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * Copyright © 2024-2025 pancho horrillo <pancho@pancho.name>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+// See ../../../readme.md
+#define PLOOPY_IGNORE_SCROLL_CLICK
+
+#define PLOOPY_DPI_OPTIONS {600}
+
+#define PLOOPY_DRAGSCROLL_MOMENTARY
+#define PLOOPY_DRAGSCROLL_DIVISOR_H 32.0
+#define PLOOPY_DRAGSCROLL_DIVISOR_V 32.0
+#define PLOOPY_DRAGSCROLL_INVERT
+
+// overrides go here!
